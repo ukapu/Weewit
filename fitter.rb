b01a4d0e6e5156da090034094f9c3747a3bb1ea5
Sam Wolfe <sam.wolfe93@gmail.com>
2013-07-04 14:35:39 -0700

    init

---
+++ fitter.rb
@@ -0,0 +1,10 @@
+class Item
+  def initializes(sizes, name)
+    @sizes = sizes
+    @name = name
+  end
+
+end
+twoseven = {:waist => 27, :rise => 9, :thigh => 10, :knee => 7, :leg_opening => 6, :inseam => 33 }
+
+apcsizes = { "27" => twoseven, "28" => twoeight, "29" => twonine }

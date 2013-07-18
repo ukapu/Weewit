8be0a7f448e3472315d978a6e35f3f79690239e8
ukapu <sam.wolfe93@gmail.com>
2013-07-18 11:48:53 -0700

    things are more rails-y now...

--- fitter.rb
+++ fitter.rb
@@ -5,6 +5,3 @@ class Item
   end
 
 end
-twoseven = {:waist => 27, :rise => 9, :thigh => 10, :knee => 7, :leg_opening => 6, :inseam => 33 }
-
-apcsizes = { "27" => twoseven, "28" => twoeight, "29" => twonine }
--- weewit.rb
+++ weewit.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
+require_relative './scraper.rb'
 
 get '/' do
   "Welcome to Weewit!"
 end
+
+
